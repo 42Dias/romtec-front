b409dc5c5f3526046f6ae3574b6415c160a213fa
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-12-02 16:16:25 -0300

    adding home screen

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,49 @@
 import Sidebar from '../../ui/Components/Sidebar/Sidebar'
 import Navbar from '../../ui/Components/Navbar/Navbar'
+import * as S from './Home.styled'
+import { FiSettings } from 'react-icons/fi'
+import image from '../../assets/obra.png'
 
 export function Home () {
   return (
     <>
       <Sidebar />
       <Navbar />
-      <h1>dashboard</h1>
+      <S.Container>
+        <S.Content>
+          <span>Bem vindo</span>
+          <h2>Veja o passo-a-passo para utilizar nosso sistema da melhor forma</h2>
+          <p>Com esse guia rápido você entenderá como nosso sistema funciona</p>
+          <img src={image} alt='' />
+        </S.Content>
+
+        <h3>Etapas</h3>
+        <S.ContainerSteps>
+          <S.ContentSteps>
+            <FiSettings size={20} />
+            <h4>Morbi vitae lorem nisl.</h4>
+            <p>Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.</p>
+          </S.ContentSteps>
+
+          <S.ContentSteps>
+            <FiSettings size={20} />
+            <h4>Morbi vitae lorem nisl.</h4>
+            <p>Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.</p>
+          </S.ContentSteps>
+
+          <S.ContentSteps>
+            <FiSettings size={20} />
+            <h4>Morbi vitae lorem nisl.</h4>
+            <p>Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.</p>
+          </S.ContentSteps>
+
+          <S.ContentSteps>
+            <FiSettings size={20} />
+            <h4>Morbi vitae lorem nisl.</h4>
+            <p>Morbi vitae lorem nisl. Sed lobortis non sapien sit amet consectetur.</p>
+          </S.ContentSteps>
+        </S.ContainerSteps>
+      </S.Container>
     </>
   )
 }
