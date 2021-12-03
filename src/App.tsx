e5bb3cf6d38401906fecad78f944403328747b1d
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-12-03 12:27:06 -0300

    adding payments screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Login, Home, Register, ForgotPassword, Terms, Error, Labor, Rods, SoilTypes, Topography, ConfigurationCrossing } from './pages'
+import { Login, Home, Register, ForgotPassword, Terms, Error, Labor, Rods, SoilTypes, Topography, ConfigurationCrossing, Payments } from './pages'
 
 import { GlobalStyle } from './ui'
-import { LOGIN, HOME, REGISTER, FORGOTPASSWORD, TERMS, LABOR, RODS, SOILTYPES, TOPOGRAPHY, CONFIGURATIONOFTHECROSSING } from './routes'
+import { LOGIN, HOME, REGISTER, FORGOTPASSWORD, TERMS, LABOR, RODS, SOILTYPES, TOPOGRAPHY, CONFIGURATIONOFTHECROSSING, PAYMENTS } from './routes'
 
 export function App () {
   return (
@@ -19,6 +19,7 @@ export function App () {
         <Route path={SOILTYPES} element={<SoilTypes />} />
         <Route path={TOPOGRAPHY} element={<Topography />} />
         <Route path={CONFIGURATIONOFTHECROSSING} element={<ConfigurationCrossing />} />
+        <Route path={PAYMENTS} element={<Payments />} />
         <Route path='*' element={<Error />} />
       </Routes>
       <GlobalStyle />
