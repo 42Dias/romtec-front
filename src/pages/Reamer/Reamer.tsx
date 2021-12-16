6a5b0fd9a7c2f0992671cae93f95c7e420067c06
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-12-16 16:47:59 -0300

    refactoring input fields in the screen reamer

--- src/pages/Reamer/Reamer.tsx
+++ src/pages/Reamer/Reamer.tsx
@@ -3,17 +3,19 @@ import Sidebar from '../../ui/Components/Sidebar/Sidebar'
 import Navbar from '../../ui/Components/Navbar/Navbar'
 import Modal from '../../ui/Components/Modal/Modal'
 
+import { TextField } from '../../ui/Components/TextField'
+import { useForm } from 'react-hook-form'
 import { FiPlus } from 'react-icons/fi'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
 
 type FormData = {
-  name: string;
-  expectedViscosity: string;
-  quantityWater: string;
-  baseQuantityFormulation: string;
-  flowLimit: string;
-  sandContent: string;
+  swivelCapacity: string;
+  conditionOfUse: string;
+  typeOfReamer: string;
+  indicationIdealGroundForWork: string;
+  numberJets: string;
+  unitOfMeasurement: string;
+  screwThread: string;
 }
 
 export function Reamer () {
@@ -21,25 +23,10 @@ export function Reamer () {
 
   const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>()
 
-  function onSubmit ({
-    name,
-    expectedViscosity,
-    quantityWater,
-    baseQuantityFormulation,
-    flowLimit,
-    sandContent,
-  }: FormData) {
-    const submit = {
-      name,
-      expectedViscosity,
-      quantityWater,
-      baseQuantityFormulation,
-      flowLimit,
-      sandContent,
-    }
-    reset()
+  function onSubmit (data: FormData) {
+    console.log(data)
 
-    console.log(submit)
+    reset()
   }
 
   return (
@@ -49,6 +36,7 @@ export function Reamer () {
       <S.ContainerConfirmation>
         <h2>Alargador</h2>
         <button onClick={() => setIsOpen(true)}><FiPlus /></button>
+
         <S.GridConfirmation>
           <span>Código de ferramenta</span>
           <span>Máquina</span>
@@ -56,125 +44,66 @@ export function Reamer () {
           <span>Capacidade de carga</span>
           <span>Descrição</span>
         </S.GridConfirmation>
-        {isOpen
-          ? <Modal onClose={() => setIsOpen(false)}>
-            <S.Container>
-              <S.Form onSubmit={handleSubmit(onSubmit)}>
 
-                <div className='grid'>
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='name'>CAPACIDADE DO SWIVEL*</label>
-                      <input
-                        id='name' placeholder='CAPACIDADE DO SWIVEL*'
-                        {...register('name', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                      <span>{errors.name?.message}</span>
-                    </fieldset>
-                  </S.ContentForm>
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          <S.Container>
+            <S.Form onSubmit={handleSubmit(onSubmit)}>
+              <TextField
+                label='Capacidade do Swivel'
+                errorMessage={errors.swivelCapacity?.message}
+                {...register('swivelCapacity', {
+                  required: {
+                    value: true,
+                    message: 'Todos os campos são obrigatórios',
+                  },
+                })}
+              />
 
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='expectedViscosity'>CONDIÇÃO DE USO*</label>
-                      <input
-                        id='expectedViscosity' placeholder='CONDIÇÃO DE USO'
-                        {...register('expectedViscosity', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                    </fieldset>
-                  </S.ContentForm>
+              <TextField
+                label='Condições de uso'
+                {...register('conditionOfUse', {
+                  required: true,
+                })}
+              />
 
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='quantityWater'>TIPO DE ALARGADOR*</label>
-                      <input
-                        id='quantityWater' placeholder='TIPO DE ALARGADOR'
-                        {...register('quantityWater', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                    </fieldset>
-                  </S.ContentForm>
+              <TextField
+                label='Tipo de alargador'
+                {...register('typeOfReamer', {
+                  required: true,
+                })}
+              />
 
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='baseQuantityFormulation'>INDICAÇÃO DE SOLO IDEAL PARA TRABALHO*</label>
-                      <input
-                        id='baseQuantityFormulation' placeholder='INDICAÇÃO DE SOLO IDEAL PARA TRABALHO'
-                        {...register('baseQuantityFormulation', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                    </fieldset>
-                  </S.ContentForm>
+              <TextField
+                label='Indicação de tipo ideal de solo para trabalho'
+                {...register('indicationIdealGroundForWork', {
+                  required: true,
+                })}
+              />
 
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='flowLimit'>NÚMERO DE JATOS *</label>
-                      <input
-                        id='flowLimit' placeholder='NÚMERO DE JATOS'
-                        {...register('flowLimit', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                    </fieldset>
-                  </S.ContentForm>
+              <TextField
+                label='Número de jatos'
+                {...register('numberJets', {
+                  required: true,
+                })}
+              />
 
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='sandContent'>OPÇÃO DE ESCOLHA DA UNIDADE DE MEDIDA PARA O DIÂMETRO </label>
-                      <input
-                        id='sandContent' placeholder='OPÇÃO DE ESCOLHA DA UNIDADE DE MEDIDA PARA O DIÂMETRO*'
-                        {...register('sandContent', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                    </fieldset>
-                  </S.ContentForm>
+              <TextField
+                label='Unidade de medida'
+                {...register('unitOfMeasurement', {
+                  required: true,
+                })}
+              />
 
-                  <S.ContentForm>
-                    <fieldset>
-                      <label htmlFor='sandContent'>ROSCA/CONEXÃO/EIXO*</label>
-                      <input
-                        id='sandContent' placeholder='ROSCA/CONEXÃO/EIXO'
-                        {...register('sandContent', {
-                          required: {
-                            value: true,
-                            message: 'Todos os campos são obrigatórios',
-                          },
-                        })}
-                      />
-                    </fieldset>
-                  </S.ContentForm>
-                </div>
-
-                <button type='submit'>Salvar</button>
-              </S.Form>
-            </S.Container>
-            {/* eslint-disable-next-line */}
-            </Modal>
-          : null}
+              <TextField
+                label='Rosca'
+                {...register('screwThread', {
+                  required: true,
+                })}
+              />
+              <button type='submit'>Salvar</button>
+            </S.Form>
+          </S.Container>
+        </Modal>
       </S.ContainerConfirmation>
     </>
   )
