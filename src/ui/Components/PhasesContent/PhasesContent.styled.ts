e50fb1290f890079884fd360db081a52cf81c009
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-01-03 09:14:18 -0300

    hide

---
+++ src/ui/Components/PhasesContent/PhasesContent.styled.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components/macro'
+import { theme } from '../../styles/theme'
+
+export const Modal = styled.div`
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Container = styled.div`
+  background: ${theme.colors.darkBlack};
+  width: 70%;
+  color: ${theme.colors.white};
+  min-height: 35%;
+  padding: 10px;
+  border-radius: 5px;
+
+  button {
+    color: ${theme.colors.white};
+    border-radius: 5px;
+    border: none;
+    font-size: 12px;
+    margin-top: 10px;
+  }
+`
